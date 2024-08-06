ff23b38788d66bd43862f8cd188ee7ec26129868
Aaron Mugume <aaronmug091@gmail.com>
2024-08-06 09:37:55 +0300

    Game Screen Commit

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
 
 function PrimaryButton({ children, onPress }) {
   return (
@@ -8,7 +9,7 @@ function PrimaryButton({ children, onPress }) {
           pressed ? [styles.buttonContainer, pressed] : styles.buttonContainer
         }
         onPress={onPress}
-        android_ripple={{ color: "#640233" }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -25,7 +26,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   buttonContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
